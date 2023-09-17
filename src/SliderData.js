68400bb42df3bcf3b7d05f8aab653873dea5859f
MohamedAlyEtriby <mohamedalyetriby@gmail.com>
2023-09-17 12:02:24 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/SliderData.js
@@ -0,0 +1,76 @@
+const slides = [
+  {
+    id: 0,
+    image: "/assets/Asset1.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    id: 1,
+
+    image: "/assets/Asset2.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    id:2,
+    image: "/assets/Asset3.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    id:3,
+    image: "/assets/Asset4.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    id:4,
+    image: "/assets/Asset5.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    id:5,
+    image: "/assets/Asset6.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    id:6,
+    image: "/assets/Asset3.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    id:7,
+    image: "/assets/Asset4.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    id: 8,
+    image: "/assets/Asset5.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+];
+export default slides;
